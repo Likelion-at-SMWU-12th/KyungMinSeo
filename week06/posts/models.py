4900b4ff9fb5db0777ad65319d846caf2a83c490
mxinseo <jade201913@gmail.com>
2024-05-13 15:08:29 +0900

    Revert : 원래 상태로 revert

--- week06/posts/models.py
+++ week06/posts/models.py
@@ -7,9 +7,3 @@ class Post (models.Model):
     contentn = models.TextField('내용')
     created_at = models.DateTimeField('작성일')
     view_count = models.IntegerField('조회수', default=0) 
-
-class Profile(models.Model):
-    profile_img = models.ImageField(verbose_name='프로필사진')
-    profile_name = models.TextField('닉네임')
-    birthday = models.DateField('생년월일')
-    profile_comment = models.TextField('한줄소개')
