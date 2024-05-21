a010218e1c4cef7ef1989ab37b9b8190c8edb1da
mxinseo <jade201913@gmail.com>
2024-05-21 20:20:12 +0900

    Feat: url_parameter_view FBV 생성

--- week06/posts/views.py
+++ week06/posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+# def url_view(request):
+#     return HttpResponse('url.view')
+
+# def url_view(request):
+#     data={'code':'001', 'msg':'OK'}
+#     return JsonResponse(data)
+
+def url_view(request):
+    data={'code':'001', 'msg':'OK'}
+    return HttpResponse('<h1>url_views</h1>')
+
+def url_parameter_view(request, username):
+    print(username)
+    return HttpResponse()
