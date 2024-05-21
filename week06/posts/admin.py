200e17501c142b01a17b73249d99fbe9111be571
mxinseo <jade201913@gmail.com>
2024-05-21 19:57:49 +0900

    Feat: 커스터마이징-게시판

--- week06/posts/admin.py
+++ week06/posts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Post , Profile
+from .models import Post , Profile, Comment
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Profile)
+admin.site.register(Comment)
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image', 'contentn', 'created_at', 'view_count', 'writer']
+    # list_editable = ['contentn', 'writer']
+    list_filter = ['created_at']
+    search_fields = ['id', 'writer__username']
+    search_help_text=['게시판번호, 작성자 검색이 가능합니다.']
