b6e78786dc389fd71478d65a508f834af30f1d2b
mxinseo <jade201913@gmail.com>
2024-05-21 19:07:32 +0900

    Feat: Comment Model 생성

--- week06/posts/models.py
+++ week06/posts/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
-class Post (models.Model):
-    image = models.ImageField(verbose_name='이미지')
-    contentn = models.TextField('내용')
-    created_at = models.DateTimeField('작성일')
-    view_count = models.IntegerField('조회수', default=0) 
+User=get_user_model()
 
-class Profile(models.Model):
-    profile_img = models.ImageField(verbose_name='프로필사진')
-    profile_name = models.TextField('닉네임')
-    birthday = models.DateField('생년월일')
-    profile_comment = models.TextField('한줄소개')
+class Post (models.Model):
+    image = models.ImageField(verbose_name='이미지', null=True, blank=True)
+    contentn = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
+    view_count = models.IntegerField(verbose_name='조회수', default=0)
+    writer=models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='작성자', null=True, blank=True)
+
+class Comment(models.Model):
+    content=models.TextField(verbose_name='내용')
+    created_at=models.DateTimeField(verbose_name='작성일', auto_now_add=True)
+    post= models.ForeignKey(to='Post',on_delete=models.CASCADE, verbose_name='게시글')
+    writer=models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='작성자', null=True)
+
+# class Profile(models.Model):
+#     profile_img = models.ImageField(verbose_name='프로필사진')
+#     profile_name = models.TextField('닉네임')
+#     birthday = models.DateField('생년월일')
+#     profile_comment = models.TextField('한줄소개')
