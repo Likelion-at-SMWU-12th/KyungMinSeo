cef3966c695a1aa08d5cbb68f13917dadd0e7239
mxinseo <jade201913@gmail.com>
2024-05-21 20:01:07 +0900

    Feat: 커스터마이징-댓글 추가

--- week06/posts/admin.py
+++ week06/posts/admin.py
@@ -6,10 +6,21 @@ from .models import Post , Profile, Comment
 admin.site.register(Profile)
 admin.site.register(Comment)
 
-@admin.register(Post)
+class CommentInLine(admin.TabularInline):   #StackedInLine도 사용 가능
+    model=Comment
+    extra=5
+    min_num=3
+    max_num=5
+    verbose_name='댓글'
+    verbose_name_plural='댓글들'
+
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'image', 'contentn', 'created_at', 'view_count', 'writer']
     # list_editable = ['contentn', 'writer']
     list_filter = ['created_at']
     search_fields = ['id', 'writer__username']
     search_help_text=['게시판번호, 작성자 검색이 가능합니다.']
+    readonly_fields=['view_count', 'created_at']
+    inlines=[CommentInLine]
+
+admin.site.register(Post, PostModelAdmin)
