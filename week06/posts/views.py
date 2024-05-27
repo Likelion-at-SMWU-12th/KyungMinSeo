3e377213d8784fd700c90534467cb9c39c1b52b2
mxinseo <jade201913@gmail.com>
2024-05-27 17:23:40 +0900

    Fix: posts/views.py의 detail/update/delete에 id 파라미터 추가

--- week06/posts/views.py
+++ week06/posts/views.py
@@ -35,16 +35,16 @@ def index(request):
 def post_list_view(request):
     return render(request, 'posts/post_list.html')
 
-def post_detail_view(request):
+def post_detail_view(request, id):
     return render(request, 'posts/post_detail.html')
 
 def post_create_view(request):
     return render(request, 'posts/post_form.html')
 
-def post_update_view(request):
+def post_update_view(request, id):
     return render(request, 'posts/post_update.html')
 
-def post_delete_view(request):
+def post_delete_view(request, id):
     return render(request, 'posts/post_confirm_delete.html')
 
 
