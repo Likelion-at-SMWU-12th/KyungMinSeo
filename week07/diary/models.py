8b66c61cdfaa4b7ccf5a9ffd5614e9a22227d287
mxinseo <jade201913@gmail.com>
2024-05-27 20:53:29 +0900

    Feat: Write_Diary 모델 생성

--- week07/diary/models.py
+++ week07/diary/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Write_Diary(models.Model):
+    image=models.ImageField(verbose_name='이미지')
+    content=models.TextField('일기')
+    created_at=models.DateTimeField('작성일')
+    
