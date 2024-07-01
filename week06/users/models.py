3638371c2b62221217f43c40681d1b22d25a64cb
mxinseo <jade201913@gmail.com>
2024-07-01 19:51:04 +0900

    Feat: User 모델 정의

--- week06/users/models.py
+++ week06/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+#hw3
+class User(AbstractUser):
+    phone = models.CharField(verbose_name='전화번호', max_length=11) # verbose : 상세한
