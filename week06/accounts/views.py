f5d5f4e13e991f307286836ca239fa4680e2d336
mxinseo <jade201913@gmail.com>
2024-07-01 21:24:50 +0900

    Feat : login 뷰, 템플릿 생성 및 url 연결

--- week06/accounts/views.py
+++ week06/accounts/views.py
@@ -14,9 +14,21 @@ def signup_view(request):
     
     else:
         form =SignUpForm(request.POST)
-
         if form.is_valid():
             instance = form.save()
             return redirect('index')
         else:
             return redirect('accounts/signup')
+        
+
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html', {'form' : AuthenticationForm})
+    
+    else:
+        form =AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.user_cache)
+            return redirect('index')
+        else:
+            return render(request, 'accounts/login.html', {'form':form})
