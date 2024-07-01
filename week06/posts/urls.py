7fc4bd0788798c55cb4bbbf0b044f5800d1aba64
mxinseo <jade201913@gmail.com>
2024-07-01 18:22:19 +0900

    Feat : urls.py에서 post_form_view의 url 정의

--- week06/posts/urls.py
+++ week06/posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import post_create_view, post_delete_view
 from .views import post_list_view, post_update_view, post_detail_view
+from .views import post_form_view
 
 app_name='posts'
 
@@ -11,4 +12,5 @@ urlpatterns=[
     path('<int:id>/', post_detail_view),
     path('<int:id>/edit', post_update_view),
     path('<int:id>/delete', post_delete_view),
+    path('form/', post_form_view, name='post-form') # hw3
 ]
