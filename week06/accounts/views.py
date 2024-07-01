968bb8dcb74475307e66b64a41250d4b36897da3
mxinseo <jade201913@gmail.com>
2024-07-01 21:20:19 +0900

    Feat: 회원가입 유효성 검사 기능 추가

--- week06/accounts/views.py
+++ week06/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
@@ -11,3 +11,12 @@ def signup_view(request):
         form = SignUpForm
         context = {'form' : form}
         return render(request, 'accounts/signup.html', context)
+    
+    else:
+        form =SignUpForm(request.POST)
+
+        if form.is_valid():
+            instance = form.save()
+            return redirect('index')
+        else:
+            return redirect('accounts/signup')
