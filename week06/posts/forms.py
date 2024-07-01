59fdf54beb494137bc8e2cb3cb67b6b42e7c36ba
mxinseo <jade201913@gmail.com>
2024-07-01 18:32:25 +0900

    Feat : forms.py에 textarea, choice 위젯 추가 및 label 추가

--- week06/posts/forms.py
+++ week06/posts/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 class PostBasedForm(forms.Form):
     image = forms.ImageField()
-    content = forms.CharField()
+    content = forms.CharField(widget=forms.Textarea)
+    CATEGORY_CHOICES = [
+        ('1', '일반'),
+        ('2', '계정'),
+    ]
+    category = forms.ChoiceField(label='카테고리', choices=CATEGORY_CHOICES)
