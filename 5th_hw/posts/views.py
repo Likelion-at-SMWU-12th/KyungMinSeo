9cdc9426d73f0f8a71652d1e88a134a7d61de272
mxinseo <jade201913@gmail.com>
2024-07-14 21:06:54 +0900

    Feat : Post, Comment ViewSet 생성

--- 5th_hw/posts/views.py
+++ 5th_hw/posts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 # Create your views here.
+
+# 게시글 뷰셋
+class PostModelViewSet(ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+
+# 댓글 뷰셋
+class CommentModelViewSet(ModelViewSet):
+    queryset=Comment.objects.all()
+    serializer_class=CommentSerializer
+
+
+
