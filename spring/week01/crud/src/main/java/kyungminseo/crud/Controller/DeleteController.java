7859b18185dfa0bce64a83b0b69eb704f1c45b8a
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-30 19:56:37 +0900

    Feat : api/v1/delete-api/ 작성

--- spring/week01/crud/src/main/java/kyungminseo/crud/Controller/DeleteController.java
+++ spring/week01/crud/src/main/java/kyungminseo/crud/Controller/DeleteController.java
@@ -1,10 +1,16 @@
 package kyungminseo.crud.Controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/delete-api")
 public class DeleteController {
+    @DeleteMapping(value = "/{variable}")
+    public String DeleteVariable(@PathVariable String variable) {
+        return variable;
+    }
 
 }
