d0841f75776a022dabbfa43b8a69c6459b9a783d
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-30 19:44:42 +0900

    Feat : PutController 생성

---
+++ spring/week01/crud/src/main/java/kyungminseo/crud/Controller/PutController.java
@@ -0,0 +1,9 @@
+package kyungminseo.crud.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+}
