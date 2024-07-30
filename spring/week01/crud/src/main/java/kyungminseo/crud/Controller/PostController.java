6c4a4eef568f6554892329ca8c4d5da2e629252b
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-30 19:38:26 +0900

    Feat : api/v1/post-api/member 작성

--- spring/week01/crud/src/main/java/kyungminseo/crud/Controller/PostController.java
+++ spring/week01/crud/src/main/java/kyungminseo/crud/Controller/PostController.java
@@ -1,9 +1,24 @@
 package kyungminseo.crud.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
 public class PostController {
+
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " " + map.getValue()+ "\n");
+        });
+        return sb.toString();
+    }
 }
