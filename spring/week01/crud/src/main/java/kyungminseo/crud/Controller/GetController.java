8c68432d1a5db6d890819ec075266b59748fe2f5
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-30 19:24:09 +0900

    Feat : api/v1/get-api/request2 작성

--- spring/week01/crud/src/main/java/kyungminseo/crud/Controller/GetController.java
+++ spring/week01/crud/src/main/java/kyungminseo/crud/Controller/GetController.java
@@ -2,6 +2,8 @@ package kyungminseo.crud.Controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("api/v1/get-api")
 public class GetController {
@@ -26,5 +28,15 @@ public class GetController {
             @RequestParam String email,
             @RequestParam String organization) {
         return name + " " + email + " " + organization;
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " " + map.getValue() + "\n");
+        });
+        return sb.toString();
     }
 }
