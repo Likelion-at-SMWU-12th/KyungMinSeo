ab0a068c79e5432e611d42d8bf866907f6542930
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-09-10 17:06:42 +0900

    Feat : Repository에 인기있는 상품 Top10을 반환하는 메서드 구현

--- querydsl/src/main/java/study/querydsl/repository/ProductRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package study.querydsl.repository;
 
+import com.fasterxml.jackson.databind.node.POJONode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.querydsl.entity.Product;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findTop10ByOrderByPopularityDesc();
 }
