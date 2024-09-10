4385e96c0f74bf811fe040e38c2403c6348c2b60
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-09-10 17:11:46 +0900

    Feat : Product 엔티티에 createdAt 필드 생성

--- querydsl/src/main/java/study/querydsl/entity/Product.java
+++ querydsl/src/main/java/study/querydsl/entity/Product.java
@@ -7,6 +7,9 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -23,10 +26,13 @@ public class Product {
     private int stock;
     private long popularity; // 인기도
 
+    private LocalDateTime createdAt; // 등록 시간
+
     public Product(String name, int price, int stock, long popularity) {
         this.name = name;
         this.price = price;
         this.stock = stock;
         this.popularity = popularity;
+        this.createdAt = LocalDateTime.now();
     }
 }
