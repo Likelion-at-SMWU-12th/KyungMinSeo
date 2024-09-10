f1fc121289fbdd663535bb6935f7d8fbc3aa8cc9
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-09-10 17:06:16 +0900

    Feat : Product 엔티티에 popularity 추가

--- querydsl/src/main/java/study/querydsl/entity/Product.java
+++ querydsl/src/main/java/study/querydsl/entity/Product.java
@@ -21,10 +21,12 @@ public class Product {
     private String name;
     private int price;
     private int stock;
+    private long popularity; // 인기도
 
-    public Product(String name, int price, int stock) {
+    public Product(String name, int price, int stock, long popularity) {
         this.name = name;
         this.price = price;
         this.stock = stock;
+        this.popularity = popularity;
     }
 }
