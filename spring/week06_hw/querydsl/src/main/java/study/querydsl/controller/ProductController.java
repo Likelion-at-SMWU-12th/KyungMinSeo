96180d7e71d04a5e3234c02a99dc00e02fc6864d
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-09-24 15:02:34 +0900

    Feat : test api 작성

--- spring/week06_hw/querydsl/src/main/java/study/querydsl/controller/ProductController.java
+++ spring/week06_hw/querydsl/src/main/java/study/querydsl/controller/ProductController.java
@@ -5,13 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.querydsl.dto.ValidRequestDto;
 import study.querydsl.dto.ValidationGroup1;
 import study.querydsl.dto.ValidationGroup2;
+import study.querydsl.exception.CustomException;
+import study.querydsl.exception.HttpStatus;
+import study.querydsl.exception.ProductException;
 
 @RestController
 @RequestMapping("/validation")
@@ -40,5 +40,10 @@ public class ProductController {
     public ResponseEntity<String> checkValidationByAllGroup(@Validated({ValidationGroup1.class, ValidationGroup2.class}) @RequestBody ValidRequestDto validRequestDto){
         LOGGER.info(validRequestDto.toString());
         return ResponseEntity.ok().body(validRequestDto.toString());
+    }
+
+    @GetMapping("product/exception")
+    public void getProductRuntimeException() throws ProductException {
+        throw new ProductException(HttpStatus.BAD_REQUEST, "ProductException 메서드 호출");
     }
 }
