46e36973af15b2dbf36b92d98b2b0c2dcd6216e3
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-09-24 19:54:06 +0900

    Feat : RestTemplateController 작성

--- spring/week07/client/src/main/java/com/example/client/controller/RestTemplateController.java
+++ spring/week07/client/src/main/java/com/example/client/controller/RestTemplateController.java
@@ -1,4 +1,22 @@
 package com.example.client.controller;
 
+import com.example.client.service.RestTemplateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest-template")
 public class RestTemplateController {
+
+    private final RestTemplateService restTemplateService;
+
+    public RestTemplateController(RestTemplateService restTemplateService) {
+        this.restTemplateService = restTemplateService;
+    }
+
+    @GetMapping
+    public String getName() { return restTemplateService.getName(); }
+
+
 }
